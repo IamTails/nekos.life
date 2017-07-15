e1a3019bc8fbeb900ec46e2997bfed4cd04c9d6d
TomsUsername <bigt62187+1@gmail.com>
2017-07-15 11:07:25 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect, request, url_for, jsonify,render_template,send_from_directory
+from flask import Flask, g, session, redirect, request, url_for, jsonify,render_template,abort,send_from_directory
 import os
 import random
 from gevent import monkey
@@ -10,14 +10,27 @@ def random_image():
     img_url = url_for('static', filename=os.path.join('neko', random.choice(names)))
     return img_url
 
+def random_lewd_image():
+    names = os.listdir(os.path.join(application.static_folder, 'nya'))
+    img_url = url_for('static', filename=os.path.join('nya', random.choice(names)))
+    return img_url
+
+
 @application.route('/')
 def neko():
-    return render_template('neko.html', img_url=random_image()
-)
+    return render_template('neko.html', img_url=random_image())
 
+@application.route('/lewd')
+def nsfwneko():
+    return render_template('nsfwneko.html', img_url=random_lewd_image())
 @application.route('/api/neko')
 def nekos():
     link = 'https://nekos.life' + random_image()
+    return jsonify(neko=link)
+
+@application.route('/api/lewd/neko')
+def lewdnekos():
+    link = 'https://nekos.life' + random_lewd_image()
     return jsonify(neko=link)
 
 if __name__ == '__main__':
