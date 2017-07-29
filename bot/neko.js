2fd87ad73e1f191e4ecf6acaff1b8c4902884996
Anoun <pertoli@mail.com>
2017-07-29 22:07:49 +0100

    Update neko.js

--- bot/neko.js
+++ bot/neko.js
@@ -18,11 +18,12 @@ fs.readdir("./events/", (err, files) => {
     });
 });
 client.on("message", async (message) => {
+    const prefix = JSON.parse(fs.readFileSync(prefixes, 'utf8'));
     if (message.author.bot) return;
-    if(message.content.indexOf(config.prefix) !== 0) return;
+    if(message.content.indexOf(prefix[message.guild.id].prefix) !== 0) return;
 
     // This is the best way to define args. Trust me.
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix[message.guild.id]prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     // The list of if/else is replaced with those simple 2 lines:
