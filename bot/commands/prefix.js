53f23e08fb1cf061deb80aed2d12af4e83a91d31
Anoun <pertoli@mail.com>
2017-07-29 22:25:49 +0100

    Update prefix.js

--- bot/commands/prefix.js
+++ bot/commands/prefix.js
@@ -1,4 +1,14 @@
 exports.run = (client, message, args) => {
+  function getRandomColor() {
+
+        let letters = '0123456789';
+        let color = '';
+        for (let i = 0; i < 7; i++) {
+            color += letters[Math.floor(Math.random() * 10)];
+        }
+
+        return color;
+    }
   const config = require("../config.json");
     const path = require('path');
     const prefixes = path.join('../prefixes.json')
@@ -18,6 +28,19 @@ exports.run = (client, message, args) => {
         msg.reply(`wew set my prefix to ${param}.`)
       }
     } else {
-    
+      message.channel.send({
+        embed: {
+            color: getRandomColor(),
+            author: {
+                name: "Prefix for " + message.guild.name
+                icon_url: message.guild.iconURL()
+            },
+            description: 'huh use ' + prefix[message.guild.id].prefix + 'help for my beautiful commands wew',
+            timestamp: new Date(),
+            footer: {
+                text: "Help requested by " + message.author.username
+            }
+        }
+    }).catch(e => console.warn('wew tf happened here ' + e))
     }
 };
