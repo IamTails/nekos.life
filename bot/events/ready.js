76341215ebb09688f50b008180e572c1dca792ec
Anoun <pertoli@mail.com>
2017-07-30 02:02:42 +0100

    Update ready.js

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -3,45 +3,29 @@
  */
 exports.run = (client) => {
     const Discord = require('discord.js');
-    const config = require("../config.json");
-    const snekfetch = require('snekfetch');
-    const dblkey = config.dblkey;
-    const dbotskey = config.dbotskey;
     const moment = require('moment');
-    const fs = require('fs')
-    const path = require('path');
-    const prefixes = path.join('./prefixes.json');
-
-    function getRandomColor() {
-
-        let letters = '0123456789';
-        let color = '';
-        for (let i = 0; i < 7; i++) {
-            color += letters[Math.floor(Math.random() * 10)];
-        }
-
-        return color;
-    }
+    const path = require("path");
+    const prefixPath = path.join(__dirname, "prefixes.json");
+    const gprefix = JSON.parse(fs.readFileSync(prefixPath));
     
-    const prefix = JSON.parse(fs.readFileSync(prefixes, 'utf8'));
     client.guilds.forEach(g => {
-        if (!prefix[g.id]) prefix[g.id] = {"prefix": config.prefix};
+        if (!prefix[g.id]) prefix[g.id] = {"prefix": client.prefix};
     });
 
-    snekfetch.post(`https://discordbots.org/api/bots/334186716770598912/stats1`)
-        .set('Authorization', dblkey)
+    client.snekfetch.post(`https://discordbots.org/api/bots/334186716770598912/stats`)
+        .set('Authorization', client.config.dblkey)
         .send({server_count: client.guilds.size})
         .then(r => console.log(r.status + ' for dbl guild count of ' + client.guilds.size))
         .catch(e => console.warn('wew tf happened here ' + e + ' for dbl post guild count of ' + client.guilds.size));
-    snekfetch.post(`https://bots.discord.pw/api/bots/334186716770598912/stats1`)
-        .set('Authorization', dbotskey)
+    client.snekfetch.post(`https://bots.discord.pw/api/bots/334186716770598912/stats`)
+        .set('Authorization', client.config.dbotskey)
         .send({server_count: client.guilds.size})
         .then(r => console.log('status : ' + r.status + ' for dbots guild count of ' + client.guilds.size))
         .catch(e => console.warn('wew tf happened here ' + e + ' for dbots post guild count of ' + client.guilds.size));
     client.user.setGame(`With Nekos \\o/`).catch(e => console.warn('wew tf happened here ' + e));
     client.channels.get("334471388289302539").send({
         embed: {
-            color: getRandomColor(),
+            color: client.getRandomColor(),
             title: "I restarted",
             author: {
                 name: client.user.username,
